9edc51635b6d9e4fcaaeb9c7cf435d5f7e3fe874
AndrickSilva <silva.andrick2001@gmail.com>
2022-09-15 17:20:50 +0530

    inital commit

---
+++ Calculator-app/js/script.js
@@ -0,0 +1,28 @@
+let string;
+
+let buttons = document.querySelectorAll(".button");
+let input = document.querySelector("input.screen");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == '=') {
+      string = eval(string);
+      input.value = string;
+    }
+    else if (e.target.innerHTML == 'RESET') {
+      string = ''
+      input.value = string;
+
+    } else if (e.target.innerHTML == 'DEL') {
+      string = input.value;
+      input.value = string.slice(0, -1);
+    }
+
+    else {
+      string = input.value;
+      console.log(e.target.innerHTML);
+      string = string + e.target.innerHTML;
+      input.value = string;
+    }
+
+  });
+});
---
+++ Card/README.md
@@ -0,0 +1,7 @@
+# Frontend Mentor - Stats preview card component
+
+[Live Preview](https://andricksilva.github.io/01_FrontendMentor_Card/)
+---
+![Design preview for the Stats preview card component coding challenge](./design/image.png)
+
+**Had fun building!** 🚀 😊
