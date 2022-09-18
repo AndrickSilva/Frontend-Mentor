5df10d7b3c1e67d724da22d9a2fbd15811994014
AndrickSilva <silva.andrick2001@gmail.com>
2022-09-18 17:37:31 +0530

    completed advice generator

--- adviceApp/src/app/app.component.ts
+++ adviceApp/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'adviceApp';
+
+
+  id: string = 'Advice';
+  quote: string = 'Click the dice bellow to generate advice';
+
+  fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+
+    this.id = `Advice ${data.slip.id}`;
+    this.quote = `${data.slip.advice}`;
+
+    console.log(data);
+  }
+
 }
