da967513fffbd1b11aea67e90e1b8619e1560fd8
AndrickSilva <silva.andrick2001@gmail.com>
2022-12-30 16:47:15 +0530

    responsive design

--- countries-api/src/components/Home.jsx
+++ countries-api/src/components/Home.jsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { AiOutlineSearch, AiOutlineDown } from 'react-icons/ai'
-const Home = () => {
-  return (
-    <div className='min-h-screen bg-darkBg px-24 py-12 '>
+import axios from 'axios'
 
-      <div className="flex justify-between items-center">
-        <div className="relative">
-          <input type="search" className='placeholder:text-gray-200 bg-darkElem mb-6 py-4 pl-20 pr-28 rounded-md' placeholder='Search for a country...' />
+
+const Home = () => {
+  const [Country, setCountry] = useState({
+
+  })
+
+  useEffect(() => {
+    loadData()
+  },[])
+
+  const loadData = async () => {
+    const data = await axios.get(`https://restcountries.com/v3.1/name/all`);
+    console.log(data.data);
+  }
+
+  const num = [1, 2, 3, 4, 5, 6];
+  return (
+    <div className='min-h-screen bg-darkBg px-4 sm:px-24 py-14 '>
+
+      <div className="flex justify-between items-center pb-14 md:flex-row flex-col gap-5">
+
+        <div className="relative ">
+          <input type="search" className='placeholder:text-gray-200 bg-darkElem py-4 pl-20 pr-10 sm:pr-28 rounded-md' placeholder='Search for a country...' />
           <AiOutlineSearch className='absolute top-4 left-7 text-gray-100 text-2xl' />
         </div>
-        <div className="relative">
+
+        <div className="relative group w-auto">
           <button name="region" className='bg-darkElem flex justify-center items-center gap-3 text-gray-200 py-4 rounded-md px-8'>Filter by Region
             <AiOutlineDown className=' text-xl  text-gray-100' />
           </button>
-          <div className="absolute mt-2 bg-darkElem min-w-full text-gray-200 py-4 rounded-md px-8 flex flex-col gap-3">
+          <div className="group-hover:flex hidden absolute mt-2 bg-darkElem min-w-full text-gray-200 py-4 rounded-md px-8 flex-col gap-3 transition duration-300 ease-in-out">
             <p value="Africa">Africa</p>
             <p value="America">America</p>
             <p value="Asia">Asia</p>
@@ -22,7 +41,21 @@ const Home = () => {
           </div>
         </div>
       </div>
-
+      <div className="grid gap-14 justify-center items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 ">
+        {
+          num.map((i) => (
+            <div className="flex flex-col w-72 bg-darkElem rounded-md overflow-hidden">
+              <img src="https://flagcdn.com/mx.svg" alt="Flag" />
+              <div className="text-darkText p-6">
+                <h1 className='text-xl pb-4 font-semibold'>United State of America</h1>
+                <p className='text-gray-300 font-light'><b className='text-gray-200'>Population:</b> 81,333.33</p>
+                <p className='text-gray-300 font-light'><b className='text-gray-200'>Region:</b> Europe</p>
+                <p className='text-gray-300 font-light'><b className='text-gray-200'>Capital:</b> Berlin</p>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
--- countries-api/src/components/Navigation.jsx
+++ countries-api/src/components/Navigation.jsx
@@ -3,8 +3,8 @@ import { BsFillMoonFill } from 'react-icons/bs'
 
 const Navigation = () => {
     return (
-        <nav className='bg-darkElem text-darkText px-24 py-6 flex justify-between'>
-            <h2 className='text-2xl font-bold'>Where in the world?</h2>
+        <nav className='bg-darkElem text-darkText px-4 sm:px-24 py-6 flex justify-between'>
+            <h2 className='text-xl sm:text-2xl font-bold'>Where in the world?</h2>
             <button className="flex justify-center items-center gap-2">
                 <BsFillMoonFill />
                 <p>Dark Mode</p>
