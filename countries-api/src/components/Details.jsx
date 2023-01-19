d67efaf4f667e769c7a65a38e2ec074611473d33
AndrickSilva <silva.andrick2001@gmail.com>
2023-01-19 13:24:38 +0530

    complete

--- countries-api/src/components/Details.jsx
+++ countries-api/src/components/Details.jsx
@@ -17,32 +17,34 @@ const Details = () => {
     }
     return (
         <div className='min-h-screen dark:bg-darkBg bg-lightBg px-4 sm:px-24 py-14'>
-            <Link to="/" className='bg-darkElem text-darkText flex justify-center gap-2 items-center w-fit py-2 px-9 rounded-lg shadow mb-24'> <IoArrowBackSharp /> Back</Link>
+            <Link to="/" className='bg-lightElem dark:bg-darkElem text-lightText dark:text-darkText flex justify-center gap-2 items-center w-fit py-2 px-9 rounded-lg shadow mb-10 lg:mb-24'> <IoArrowBackSharp /> Back</Link>
             {Details.map((i, index) =>
-                <div key={index} className="flex flex-row gap-44">
-                    <img className='h-72' src={i.flags[0]} alt={i.name.common} />
+                <div key={index} className="flex flex-col gap-10 xl:gap-14 lg:gap-44 lg:flex-row">
+                    <img className='h-auto lg:h-72 shadow-lg w-fit' src={i.flags[0]} alt={i.name?.common} />
                     <div className="flex flex-col">
-                        <h1 className='text-darkText font-bold text-4xl'> {i.name.common} </h1>
-                        <div className="flex flex-wrap">
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Native Name:</b> {i.name.nativeName.nld.common}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Population:</b> {i.population}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Region:</b> {i.region}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Sub Region:</b> {i.subregion}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Capital:</b> {i.capital}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Top Level Domain:</b> {i.tld}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Currencies:</b> {i.currencies.EUR.name}</p>
-                            <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Languages:</b> {i.languages.deu}</p>
+                        <h1 className='text-lightText dark:text-darkText font-bold text-4xl'> {i.name?.common} </h1>
+                        <div className="flex flex-col lg:flex-row gap-10 my-10 lg:gap-40">
+                            <div className="flex flex-col gap-2 w-fit">
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Native Name:</b> {i?.name?.common}</p>
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Population:</b> {i?.population}</p>
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Region:</b> {i?.region}</p>
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Sub Region:</b> {i?.subregion}</p>
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Capital:</b> {i?.capital}</p>
+                            </div>
+                            <div className="flex flex-col w-fit gap-2">
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Top Level Domain:</b> {i?.tld}</p>
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Currencies:</b> {i?.currencies?.EUR?.name}</p>
+                                <p className='dark:text-gray-300 text-gray-700 font-light'><b className='dark:text-gray-200 text-gray-800'>Languages:</b> {Object.values(i.languages)}</p>
+                            </div>
                         </div>
-                        <p className='dark:text-gray-300 text-gray-700 font-light'>
-                            <span className='bg-darkElem text-darkText flex justify-center gap-2 items-center w-fit py-2 px-9 rounded-lg shadow mb-24'> France </span>
-                            <span className='bg-darkElem text-darkText flex justify-center gap-2 items-center w-fit py-2 px-9 rounded-lg shadow mb-24'> France </span>
-                            <span className='bg-darkElem text-darkText flex justify-center gap-2 items-center w-fit py-2 px-9 rounded-lg shadow mb-24'> France </span>
-                        </p>
+                        <h2 className='dark:text-gray-200 text-gray-800 mb-4'>Border Countries:</h2>
+                        <div className='dark:text-gray-300 text-gray-700 font-light flex gap-2 w-fit rounded-lg mb-24 flex-wrap'>
+                            {i.borders.map(i => <span className='bg-lightElem dark:bg-darkElem text-lightText dark:text-darkText rounded-md py-2 px-6 shadow'> {i}</span>)}
+                        </div>
                     </div>
                 </div>
             )}
         </div>
-
     )
 }
 
